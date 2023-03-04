732c4aa7ccf0b408ff86b295e5e1785fde1e19dc
thembelani <thembelanim696@gmail.com>
2023-03-04 00:51:46 -0800

    a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* This a header file containing declarations
+ * This is like creating my own library with declared function
+ */
+
+/*enclosing the files  contentinbetween ifndef,define and endif,		      
+stops the compiler from processing the file twice if its included twice,
+if not so, this will cause an error*/
+
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+int _putchar(char c);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
